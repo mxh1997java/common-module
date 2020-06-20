31687910df91e0bda0408d8e02b5aca60e38dc8b
15735400536 <18334698204@163.com>
2020-06-20 22:52:04 +0800

    提交代码

---
+++ common-share/src/main/java/www/maxinhai/com/commonshare/CommonShareApplication.java
@@ -0,0 +1,13 @@
+package www.maxinhai.com.commonshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonShareApplication.class, args);
+    }
+
+}
---
+++ common-share/src/test/java/www/maxinhai/com/commonshare/CommonShareApplicationTests.java
@@ -0,0 +1,13 @@
+package www.maxinhai.com.commonshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
